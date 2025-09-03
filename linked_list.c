3e0fdc8d830f37e0631c6895a5851fd1d654d5db
Gabriel Lima <04lima.gabriel@gmail.com>
2025-09-02 21:24:44 -0300

    rafactor: modularize code

--- linked_list.c
+++ linked_list.c
@@ -6,41 +6,62 @@ struct node {
     struct node *next;
 };
 
-int main() {
+void insert_at_beginning(){
     struct node *new_node = NULL;
     struct node *head = NULL;
+
+    new_node = (struct node *) malloc(sizeof(struct node));
+
+    printf("Enter data u want to insert: ");
+    scanf("%d", &new_node->data);
+
+    if(head == NULL) {
+        head = new_node; 
+    } else {
+        new_node->next = head;
+        head = new_node; 
+    }
+}
+void insert_at_end(){}
+void insert_after_a_giver_locate(){}
+
+int main() {
+    struct node *head = NULL;
+    struct node *new_node = NULL;
     struct node *temp = NULL;
     int choice = 1;
     int count = 0;
 
-    while (choice == 1) {
+    while (choice != 0) {
         new_node = (struct node *) malloc(sizeof(struct node));
 
         printf("Enter data: ");
         scanf("%d", &new_node->data);
 
-        new_node->next = 0;
+        new_node->next = NULL;
 
         if (head == NULL) {
-            head = temp = new_node;
+            head = temp = new_node; 
         } else {
-            temp->next = new_node; 
+            temp->next = new_node;
             temp = new_node;
         }
 
-        printf("Do you want to continue(0, 1)? ");
+        printf("Do you want continue(0, 1)? ");
         scanf("%d", &choice);
         count++;
     }
-    
+    insert_at_beginning();    
+
     temp = head;
     while (temp != NULL) {
         printf("%d ", temp->data);
         temp = temp->next;
     }
-    printf("Size: %d", count);
-    
+    printf("\nSize: %d", count);
+
     free(new_node);
     new_node = NULL;
+
     return 0;
 }

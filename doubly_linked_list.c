3dd2265aee34acb3719b97b359afa288695e667e
Gabriel Lima <04lima.gabriel@gmail.com>
2025-09-15 14:17:22 -0300

    feat: implement function to create a new node

--- doubly_linked_list.c
+++ doubly_linked_list.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 struct node {
     int data;
@@ -7,3 +8,21 @@ struct node {
 };
 
 struct node *head = NULL;
+struct node *temp = NULL;
+
+void create(struct node **head, struct node **temp) {
+    struct node *new_node = (struct node *) malloc(sizeof(struct node));
+
+    printf("Enter data:");
+    scanf("%d", &new_node->data);
+    new_node->prev = NULL;
+    new_node->next = NULL;
+
+    if(*head == NULL) {
+        *head = *temp = new_node;
+    } else {
+        (*temp)->next = new_node;
+        new_node->prev = *temp;
+        *temp = new_node;
+    }
+}
